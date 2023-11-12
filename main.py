4d8efa29e0b7b1a96156c17af33fe625db55d1eb
Ronis <65214437+Ronis123@users.noreply.github.com>
2023-11-12 01:57:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,18 @@ import sys
 ##########################################################
 
 #function definition
-def V(x):
+#def V(x):
   #Potential in which the particle exists.
-  L=1
-  if x<1.0:
-    return 0  #square well
-  else:
-    return v0
+#  L=1
+ # if x<1.0:
+  #  return 0  #square well
+#  else:
+ #   return v0
 # We can then define the ODE for solution with odeint():
-def SE(y,x):
-  
-#Returns derivs for the 1−D TISE, for use in odeint.
-#Requires global value E to be set elsewhere .
-#Note that we are using x as time here . . . Python doesn’t care !
-
-  g0 = y [ 1 ]
-  g1 = 2.0∗(V(x)−E)∗y[0]
-  return array ([g0, g1])
+#def SE(y,x):
+ # g0 = y [ 1 ]
+ # g1 = 2.0∗(V(x)−E)∗y[0]
+ # return array ([g0, g1])
 
   ############################################################
 
@@ -54,21 +49,21 @@ def Final Value (energy):
 #C a l c u l a t e s p s i f o r t h i s v a l u e o f E , and
 #r e t u r n s t h e v a l u e o f p s i a t p o i n t b t o c heck d i v e r g e n c e .
 #####################################
-g loba l y
-g loba l E
-E = ene r gy
-y = o d ei n t (SE , yo , x )
-return y[ −1 , 0 ] # r e t u r n f i n a l v a l u e ( p s i a t b )
-y = z e r o s ( [ s t e p s , 2 ] )
-yo = a r r a y ( [ 1 . 0 , 0 . 0 ] ) # i n i t i a l p s i and p s i−d o t .
-x = l i n s p a c e ( 0 , b , s t e p s )
-E1 = f l o a t ( s y s . argv [ 1 ] )
-E2 = f l o a t ( s y s . argv [ 2 ] )
-answer = b ren tq ( Fi n al V al u e , E1 , E2 ) # use b r e n t q t o s o l v e f o r E−> p s i=0 a t b .
+  global y
+  global E
+  E = energy
+  y = odeint(SE, yo, x )
+  return y[−1, 0]                     # returnfinal value(psi at b)
+y = zeros ([steps ,2])
+yo = array([1.0 , 0.0])       # i n i t i a l p s i and p s i−d o t .
+x = linspace(0, b ,steps)
+E1 = float(sys.argv[ 1 ])
+E2 = float(sys.argv[ 2 ])
+answer = brentq( Final_Value, E1, E2 )     # use b r e n t q t o s o l v e f o r E−> p s i=0 a t b .
 
-pr int ’ Ei g e n v al u e found a t E = %.8 f ’ % answer
-pl o t ( x , y [ : , 0 ] )
-x l a b e l ( ” P o si ti o n ( Uni t s o f L) ” )
-show ( )
+print("Eigenvalue found at E = %.8 f" % answer)
+plot(x , y[ : , 0 ])
+xlabel(" Position(Units of L) ")
+show()
   
     
